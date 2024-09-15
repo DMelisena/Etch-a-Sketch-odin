242efeb15f35b27d6bc876e45af3c49a9fca764d
DMelisena <demonlordx777@gmail.com>
2024-09-15 13:49:29 +0700

    tidying and commenting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const drawingPad =document.querySelector(".drawing-pad")
 var grid=16
-for (let i = 0;i<grid;i++){
-  console.log("loop working",i)
+for (let i = 0;i<grid;i++){//first loop determines pixel number on y axis
+  //console.log("loop working",i)
   let drawPixelVer= document.createElement("div")
   drawPixelVer.classList.add("pixelVer")
   for (let l = 0;l<grid;l++){
-    console.log("drawPixelHor",i,l)
+   // console.log("drawPixelHor",i,l)
     let drawPixel= document.createElement("div")
     drawPixel.classList.add("pixel")
     drawPixel.addEventListener("mouseover",()=> {
